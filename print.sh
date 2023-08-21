62b8c0b35ac0bfaf4d592d9e30351fa755c6e202
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-21 17:18:14 +0530

    Create print.sh

---
+++ print.sh
@@ -0,0 +1,24 @@
+#reset - 0
+
+#red - 31
+
+#green - 32
+
+#yellow - 33
+
+#blue - 34
+
+#magenta - 35
+
+#cyan - 36
+
+#syntax:
+#echo -e "\e[CODEmMessage\e[0m"
+
+echo Hello world
+echo -e "\e[31mHello Devops\e[0m"
+echo -e "\e[32mHello Devops\e[0m"
+echo -e "\e[33mHello Devops\e[0m"
+echo -e "\e[34mHello Devops\e[0m"
+echo -e "\e[35mHello Devops\e[0m"
+echo -e "\e[36mHello Devops\e[0m"
