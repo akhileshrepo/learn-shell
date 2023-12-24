df81e1d674c41c2b0a11c93f8da5a9d0290701be
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-24 10:44:05 +0530

    updated code

--- 07-loops.sh
+++ 07-loops.sh
@@ -5,11 +5,9 @@
 a=10
 while [ $a -gt 0 ]; do
   echo Hello
-  a=$(($a-1))
+  #a=$(($a-1))
   #break # this command can break the loop
 done
-
-# Based on Inputs
-for comp in frontend catalogue user ; do
-  echo Installing Component - $comp
-done
+#for comp in frontend catalogue user ; do
+#  echo Installing Component - $comp
+#done
