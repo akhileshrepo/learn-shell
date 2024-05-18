44c987f69281d6a9e687a91102ef7cd955e886b3
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-18 12:07:17 +0530

    updated code

--- 08-quotes.sh
+++ 08-quotes.sh
@@ -1,1 +1,6 @@
-echo '**Hello World**'
+
+
+echo **Hello world**
+echo '**Hello world**'       ##If you're passing input with any command, that input has special character's then you need to quote them
+a=10 ; echo 'value of a=$a'
+a=10 ; echo "value of a=$a"  ## while accessing variables, you need to use the double quotes only
