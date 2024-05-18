2f6bf6db039a3c9a0a626024ed8ae7a404629631
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-18 11:51:47 +0530

    updated code

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,13 +1,20 @@
-# Two basic loops
-# for and while
+## Two basic loops
+## for and while
+#
+## Based on expression
+#a=10
+#while [ $a -gt 0 ]; do
+#  echo Hello
+#  a=$(($a-1))
+#  #break # this command can break the loop
+#done
+#for comp in frontend catalogue user ; do
+#  echo Installing Component - $comp
+#done
 
-# Based on expression
-a=10
-while [ $a -gt 0 ]; do
-  echo Hello
-  a=$(($a-1))
-  #break # this command can break the loop
-done
-for comp in frontend catalogue user ; do
-  echo Installing Component - $comp
+
+#for loop
+
+for comp in frontend catalogue mongodb;do
+  echo Installing component - $comp
 done
