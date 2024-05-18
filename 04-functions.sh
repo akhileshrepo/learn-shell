6beced0aa05a723ab5123aaf239a3111ad098b14
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-18 11:35:47 +0530

    updated code

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,23 +1,31 @@
-greeting() {
-  echo Hello, Good Mornign
-  echo Welcome to DevOps Training
-  return 8
-  echo Good to Have you hear.
-}
+#greeting() {
+#  echo Hello, Good Mornign
+#  echo Welcome to DevOps Training
+#  return 8
+#  echo Good to Have you hear.
+#}
+#
+#
+#greeting
+#echo Function Exit Status - $?
+#
+## You declare var in main program, you can access that in function and vice-versa.
+## Function have its own special variables
+#
+#input() {
+#  echo First Argument - $1
+#  echo Second Argument - $2
+#  echo All Arguments - $*
+#  echo No of Arguments - $#
+#  echo script name - $0
+#}
+#
+#input abc 1234
 
+
+greeting() {
+  echo "Hello Good Morning"
+  echo "Welcome to devOps"
+}
 
 greeting
-echo Function Exit Status - $?
-
-# You declare var in main program, you can access that in function and vice-versa.
-# Function have its own special variables
-
-input() {
-  echo First Argument - $1
-  echo Second Argument - $2
-  echo All Arguments - $*
-  echo No of Arguments - $#
-  echo script name - $0
-}
-
-input abc 1234
