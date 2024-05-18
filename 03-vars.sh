b583d0784ca9058da6887949f22ff0f47999a7d4
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-18 11:30:00 +0530

    updated code

--- 03-vars.sh
+++ 03-vars.sh
@@ -49,5 +49,5 @@ echo "Total Sum is $ADD"
 export XDG_SESSION_ID=5
 echo XDG_SESSION_ID - $XDG_SESSION_ID
 
-export abc=500
-echo "value of abc is $abc"
+export xyz=1000
+echo "value of abc is $xyz"
