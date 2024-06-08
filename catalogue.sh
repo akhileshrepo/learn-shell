b67ba3592e3b994dcfcbdab2b6c6cc7aa45bd1b8
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-08 13:40:25 +0530

    updated code

--- catalogue.sh
+++ catalogue.sh
@@ -12,7 +12,7 @@ cd /app
 npm install
 
 dnf install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
 
 systemctl daemon-reload
 systemctl enable catalogue
