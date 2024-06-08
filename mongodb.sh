657890166af0131e147e1fa81b95749a3b914e97
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-08 12:53:54 +0530

    updated code

--- mongodb.sh
+++ mongodb.sh
@@ -2,5 +2,6 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y
 #update listen address from 127.0.0.1 to 0.0.0.0
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
--- redis.sh
+++ redis.sh
@@ -2,6 +2,8 @@ dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
 #update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 
 systemctl enable redis
 systemctl restart redis
