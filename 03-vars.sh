9c020c44ad81dad8f303ee42af9df901c293e717
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-10 12:34:20 +0530

    updated code

--- 03-vars.sh
+++ 03-vars.sh
@@ -2,30 +2,35 @@
 a=10
 echo a is  $a
 
-# Special Variables
-# $0 - $N, $*, $#
 
-echo file name - $0
-echo first argument - $1
-echo second argument - $2
-echo no. of arguments - $#
-echo all arguments - $*
 
-# Substitution Variables
-## Command Substitution
-DATE=$(date)
 
-echo Today Date is $DATE
 
-## Arithmetic Substitution
-ADD=$(( 2+2 ))
-echo ADD of 2+2 = $ADD
 
-# Access environment variables
-echo Username - $USER
-export abc=500
-echo Env Var abc - $abc
-# export abc=100 from CLI can make this variable printed
+## Special Variables
+## $0 - $N, $*, $#
+#
+#echo file name - $0
+#echo first argument - $1
+#echo second argument - $2
+#echo no. of arguments - $#
+#echo all arguments - $*
+#
+## Substitution Variables
+### Command Substitution
+#DATE=$(date)
+#
+#echo Today Date is $DATE
+#
+### Arithmetic Substitution
+#ADD=$(( 2+2 ))
+#echo ADD of 2+2 = $ADD
+#
+## Access environment variables
+#echo Username - $USER
+#export abc=500
+#echo Env Var abc - $abc
+## export abc=100 from CLI can make this variable printed
 
 
 
