30db7b0e9ad83aa24ef83b9d2bef51c7b5000771
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-10 12:53:24 +0530

    updated code

--- user.sh
+++ user.sh
@@ -1,19 +1,4 @@
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
+component=user
+source common.sh
 
-dnf install mongodb-org-shell -y
-mongo --host mongodb.akhildevops.online </app/schema/user.js
-
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+nodejs
