acff0911d875d8046db8e5272aa929bcd32f871b
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-10 20:05:52 +0530

    updated code

--- common.sh
+++ common.sh
@@ -8,103 +8,86 @@ func_exit_status() {
   fi
 }
 
-
 func_apppreq() {
-  echo -e "\e[35m>>>>> create ${component} service <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create ${component} Service  <<<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   func_exit_status
 
-  echo -e "\e[35m>>>>> Adding roboshop user <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application User  <<<<<<<<<<<<\e[0m"
   id roboshop &>>${log}
   if [ $? -ne 0 ]; then
     useradd roboshop &>>${log}
   fi
   func_exit_status
 
-  echo -e "\e[35m>>>>> cleanup the directory <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Cleanup Existing Application Content  <<<<<<<<<<<<\e[0m"
   rm -rf /app &>>${log}
   func_exit_status
 
-  echo -e "\e[35m>>>>> change the directory <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application Directory  <<<<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
   func_exit_status
 
-  echo -e "\e[35m>>>>> Download the content <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Download Application Content  <<<<<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
   func_exit_status
 
-  echo -e "\e[35m>>>>> change the directory <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
   cd /app
-  func_exit_status
-
-  echo -e "\e[35m>>>>> Extract the content <<<<<<\e[0m" | tee -a ${log}
   unzip /tmp/${component}.zip &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>> change the directory <<<<<<\e[0m" | tee -a ${log}
-  cd /app
   func_exit_status
 }
 
 func_systemd() {
-  echo -e "\e[35m>>>>> Reload the config <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Start ${component} Service  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
   systemctl daemon-reload &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>> Enable the service <<<<<<\e[0m" | tee -a ${log}
   systemctl enable ${component} &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>> Restart the service <<<<<<\e[0m" | tee -a ${log}
   systemctl restart ${component} &>>${log}
   func_exit_status
 }
 
-
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
-    echo -e "\e[35m>>>>> Install Mongodb <<<<<<\e[0m" | tee -a ${log}
-    dnf install mongodb-org-shell -y &>>${log}
+    echo -e "\e[36m>>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    yum install mongodb-org-shell -y &>>${log}
     func_exit_status
 
-    echo -e "\e[35m>>>>> Load schema <<<<<<\e[0m" | tee -a ${log}
-    mongo --host mongodb.akhildevops.online </app/schema/${component}.js &>>${log}
+    echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    mongo --host 172.31.28.187 </app/schema/${component}.js
     func_exit_status
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
-    echo -e "\e[35m>>>>> Install Mysql client <<<<<<\e[0m" | tee -a ${log}
-    dnf install mysql -y &>>${log}
+    echo -e "\e[36m>>>>>>>>>>>>  Install MySQL Client   <<<<<<<<<<<<\e[0m"
+    yum install mysql -y &>>${log}
     func_exit_status
 
-    echo -e "\e[35m>>>>> Load schema <<<<<<\e[0m" | tee -a ${log}
-    mysql -h mysql.akhildevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+    echo -e "\e[36m>>>>>>>>>>>>  Load Schema   <<<<<<<<<<<<\e[0m"
+    mysql -h 172.31.31.224 -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
     func_exit_status
   fi
+
 }
 
-func_nodejs () {
+func_nodejs() {
   log=/tmp/roboshop.log
 
-  echo -e "\e[35m>>>>> Download mongo repos <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create MongoDB Repo  <<<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
   func_exit_status
 
-  echo -e "\e[35m>>>>> Disable Nodejs module <<<<<<\e[0m" | tee -a ${log}
-  dnf module disable nodejs -y &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS Repos  <<<<<<<<<<<<\e[0m"
+  dnf module disable nodejs -y
+  dnf module enable nodejs:18 -y
+ func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS  <<<<<<<<<<<<\e[0m"
+  dnf install nodejs -y
   func_exit_status
 
-  echo -e "\e[35m>>>>> Enable Nodejs module <<<<<<\e[0m" | tee -a ${log}
-  dnf module enable nodejs:18 -y &>>${log}
-  func_exit_status
+  func_apppreq
 
-  echo -e "\e[35m>>>>> Install Nodejs <<<<<<\e[0m" | tee -a ${log}
-  dnf install nodejs -y &>>${log}
-  func_exit_status
-
- func_apppreq
-
-  echo -e "\e[35m>>>>> Install dependencies <<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Download NodeJS Dependencies  <<<<<<<<<<<<\e[0m"
   npm install &>>${log}
   func_exit_status
 
@@ -114,34 +97,32 @@ func_nodejs () {
 }
 
 func_java() {
-
-  echo -e "\e[35m>>>>> Install Maven Packages <<<<<<\e[0m" | tee -a ${log}
-  dnf install maven -y &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Install Maven   <<<<<<<<<<<<\e[0m"
+  yum install maven -y &>>${log}
   func_exit_status
 
   func_apppreq
 
-  echo -e "\e[35m>>>>> Build ${component} service<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
   mvn clean package &>>${log}
-  mv target/shipping-1.0.jar ${component}.jar &>>${log}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log}
   func_exit_status
 
   func_schema_setup
 
   func_systemd
-
 }
 
 func_python() {
-  echo -e "\e[35m>>>>> Build ${component} service <<<<<<\e[0m" | tee -a ${log}
-  dnf install python36 gcc python3-devel -y &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
+  yum install python36 gcc python3-devel -y &>>${log}
   func_exit_status
 
   func_apppreq
 
   sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
 
-  echo -e "\e[35m>>>>> Build ${component} service<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
   func_exit_status
 
