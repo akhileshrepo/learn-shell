d589ffbc916c3967b8f715144bc2e26cc6d2e55d
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-10 14:36:04 +0530

    updated code

--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,4 @@
-cp shipping.service /etc/systemd/system/shipping.service
-dnf install maven -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+component=shipping
+source common.sh
 
-dnf install mysql -y
-mysql -h mysql.akhildevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+func_java
