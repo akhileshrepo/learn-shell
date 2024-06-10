d4e66bd59b47b9132bf649bac5758d8c9e701917
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-10 16:53:29 +0530

    updated code

--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,30 @@
 source common.sh
 
 echo -e "\e[35m>>>>> Install Nginx <<<<<<\e[0m" | tee -a ${log}
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/roboshop.log
 func_exit_status
 
 echo -e "\e[35m>>>>> Copy Roboshop configuration<<<<<<\e[0m" | tee -a ${log}
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 func_exit_status
 
 echo -e "\e[35m>>>>> Remove the Existing content <<<<<<\e[0m" | tee -a ${log}
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 func_exit_status
 
 echo -e "\e[35m>>>>> Download app content <<<<<<\e[0m" | tee -a ${log}
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 func_exit_status
 
 echo -e "\e[35m>>>>> Change the directory <<<<<<\e[0m" | tee -a ${log}
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
 func_exit_status
 
 echo -e "\e[35m>>>>> Unzip the app content <<<<<<\e[0m" | tee -a ${log}
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 func_exit_status
 
 echo -e "\e[35m>>>>> Reload and restart the service <<<<<<\e[0m" | tee -a ${log}
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
 func_exit_status
