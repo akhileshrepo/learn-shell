4d0ec4830065e52953cc3a491ca77f2e73f95405
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-10 10:10:17 +0530

    updated code

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,51 @@
+echo ">>>>>> create catalogue service <<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo ">>>>>> Download mongo repos <<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo ">>>>>> Disable Nodejs module <<<<<<"
 dnf module disable nodejs -y
+
+echo ">>>>>> Enable Nodejs module <<<<<<"
 dnf module enable nodejs:18 -y
+
+echo ">>>>>> Install Nodejs <<<<<<"
 dnf install nodejs -y
+
+echo ">>>>>> Adding roboshop user <<<<<<"
 useradd roboshop
+
+echo ">>>>>> change the directory <<<<<<"
 mkdir /app
+
+echo ">>>>>> Download the content <<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>> change the directory <<<<<<"
 cd /app
+
+echo ">>>>>> Extract the content <<<<<<"
 unzip /tmp/catalogue.zip
+
+echo ">>>>>> change the directory <<<<<<"
 cd /app
+
+echo ">>>>>> Install dependencies <<<<<<"
 npm install
 
+echo ">>>>>> Install Mongodb <<<<<<"
 dnf install mongodb-org-shell -y
+
+echo ">>>>>> Load schema <<<<<<"
 mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
 
+echo ">>>>>> Reload the config <<<<<<"
 systemctl daemon-reload
+
+echo ">>>>>> Enable the service <<<<<<"
 systemctl enable catalogue
+
+echo ">>>>>> Restart the service <<<<<<"
 systemctl restart catalogue
 
