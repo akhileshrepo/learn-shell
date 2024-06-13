9912c7497b6a460b486f42c4b3680522e0ddd043
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-13 13:21:06 +0530

    updated code

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,13 @@
+echo -e "\e[36m>>>>>>>> copy Mongo repo file <<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo -e "\e[36m>>>>>>>> Install Mongodb <<<<<<<<<<<\e[0m"
 dnf install mongodb-org -y
+
+echo -e "\e[36m>>>>>>>> Update Listen address <<<<<<<<<<<\e[0m"
 #update listen address from 127.0.0.1 to 0.0.0.0
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>> Restart the Mongodb <<<<<<<<<<<\e[0m"
 systemctl enable mongod
 systemctl restart mongod
