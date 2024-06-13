715f01bf679a48c0fe5166a29057e4af71de9fc1
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-13 18:33:53 +0530

    updated code

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,22 @@
-mysql_root_password=$1
-if [ -z "${mysql_root_password}" ]; then
-  echo "Input password missing"
-  exit 1
-fi
+source common.sh
 
-dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf install mysql-community-server -y
-systemctl enable mysqld
-systemctl restart mysqld
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Disable mysql module <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+dnf module disable mysql -y &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>> Download the repo files <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Install mysql <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+dnf install mysql-community-server -y &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+systemctl enable mysqld &>> /tmp/roboshop.log
+systemctl start mysqld &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>> Set Root Password for mysql <<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+mysql_secure_installation --set-root-pass RoboShop@1 &>> /tmp/roboshop.log
+func_exit_status
