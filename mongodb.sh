e165cc57947b8daed005101c3992371cdbadbb4e
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-13 13:26:56 +0530

    updated code

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
 echo -e "\e[36m>>>>>>>> copy Mongo repo file <<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Install Mongodb <<<<<<<<<<<\e[0m"
-dnf install mongodb-org -y
+dnf install mongodb-org -y /tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Update Listen address <<<<<<<<<<<\e[0m"
 #update listen address from 127.0.0.1 to 0.0.0.0
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf /tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>> Restart the Mongodb <<<<<<<<<<<\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod /tmp/roboshop.log
+systemctl restart mongod /tmp/roboshop.log
