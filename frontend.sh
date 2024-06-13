ac2edcfd43c6f10ff7288f06d4f47e1d847bc8cf
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-13 13:12:03 +0530

    updated code

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,25 @@
 source common.sh
 log=/tmp/roboshop.log
 
-echo "\e[35m>>>>>>>>>>>>>>> Copy roboshop configuration >>>>>>>>>>>>>>>\e[0m"| tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> Copy roboshop configuration >>>>>>>>>>>>>>>\e[0m"| tee -a /tmp/roboshop.log
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 
-echo "\e[35m>>>>>>>>>>>>>>> Install Nginx >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> Install Nginx >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
 dnf install nginx -y &>>${log}
 
-echo "\e[35m>>>>>>>>>>>>>>> Clean up Existing content >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> Clean up Existing content >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
 rm -rf /usr/share/nginx/html/* &>>${log}
 
-echo "\e[35m>>>>>>>>>>>>>>> Download the application content >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> Download the application content >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 
-echo "\e[35m>>>>>>>>>>>>>>> change the directory >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> change the directory >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
 cd /usr/share/nginx/html &>>${log}
 
-echo "\e[35m>>>>>>>>>>>>>>> Extract the content >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> Extract the content >>>>>>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
 unzip /tmp/frontend.zip &>>${log}
 
-echo "\e[35m>>>>>>>>>>>>>>> Restart the application >>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>>>>> Restart the application >>>>>>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
 systemctl restart nginx &>>${log}
 systemctl enable nginx &>>${log}
 systemctl status nginx &>>${log}
