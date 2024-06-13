81b27e1767e579d3c3eae82d7242c81850205e3f
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-13 13:49:07 +0530

    updated code

--- catalogue.sh
+++ catalogue.sh
@@ -1,52 +1,52 @@
 source common.sh
 
-echo -e "\e[36m>>>>>>>>>>>>> Copy catalogue configuration <<<<<<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m>>>>>>>>>>>>> Copy catalogue configuration <<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>> Disable and Enable Nodejs module >>>>>>>>>>>\e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+echo -e "\e[36m>>>>>>>>>>>>> Disable and Enable Nodejs module >>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+dnf module disable nodejs -y &>> /tmp/roboshop.log
+dnf module enable nodejs:18 -y &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>> Install Nodejs >>>>>>>>>>>>>>>\e[0m"
-dnf install nodejs -y
+echo -e "\e[36m>>>>>>>>>>>>>> Install Nodejs >>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+dnf install nodejs -y &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>> Adding user >>>>>>>>>>>>>>>\e[0m"
-useradd roboshop
+echo -e "\e[36m>>>>>>>>>>>> Adding user >>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+useradd roboshop &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>>> Create app directory >>>>>>>>>>>>>>\e[0m"
-mkdir /app
+echo -e "\e[36m>>>>>>>>>>>>>>> Create app directory >>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+mkdir /app &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>> Download app content >>>>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[36m>>>>>>>>>>>>>> Download app content >>>>>>>>>>>>>>>>\e[0m" | tee -a /tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>> Extract the content <<<<<<<<<<<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/catalogue.zip
+echo -e "\e[36m>>>>>>>>>>>>>> Extract the content <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cd /app &>> /tmp/roboshop.log
+unzip /tmp/catalogue.zip &>> /tmp/roboshop.log
 func_exit_status
 
-echo _e "\e[36m>>>>>>>>>>>>> Install Dependencies <<<<<<<<<<<<<<<<<<<<<\e[0m"
-cd /app
-npm install
+echo _e "\e[36m>>>>>>>>>>>>> Install Dependencies <<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cd /app &>> /tmp/roboshop.log
+npm install &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>Restart the service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+echo -e "\e[36m>>>>>>>>>>>>Restart the service <<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+systemctl daemon-reload &>> /tmp/roboshop.log
+systemctl enable catalogue &>> /tmp/roboshop.log
+systemctl start catalogue &>> /tmp/roboshop.log
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>>>>Download mongo repo file <<<<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>>>>>>>>Download mongo repo file <<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
 
-echo -e "\e[36m>>>>>>>>>>>>>>>>Install Mongodb client<<<<<<<<<<<<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y
+echo -e "\e[36m>>>>>>>>>>>>>>>>Install Mongodb client<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+dnf install mongodb-org-shell -y &>> /tmp/roboshop.log
 
-echo -e "\e[36m>>>>>>>>>>>>>> Load schema<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+echo -e "\e[36m>>>>>>>>>>>>>> Load schema<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js &>> /tmp/roboshop.log
 
