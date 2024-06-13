e2f7242ab3fa62514a663aaafe36e8a566199611
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-13 14:58:28 +0530

    updated code

--- redis.sh
+++ redis.sh
@@ -1,9 +1,24 @@
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-dnf install redis -y
-#update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+source common.sh
 
-systemctl enable redis
-systemctl restart redis
+
+echo -e "\e[36m>>>>>>>>>>>> Download redis repos <<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>> enable redis module <<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+dnf module enable redis:remi-6.2 -y &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>> Install redis <<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+dnf install redis -y &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>> update listen address <<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf &>> /tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf &>> /tmp/roboshop.log
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+systemctl enable redis &>> /tmp/roboshop.log
+systemctl restart redis &>> /tmp/roboshop.log
+func_exit_status
--- user.sh
+++ user.sh
@@ -1,4 +1,1 @@
-component=user
-source common.sh
-schema_type=mongodb
-func_nodejs
+
